42c69702cdaede5a8928894fe0609c8f77149423
nick <nick@nickdown.com>
2020-10-19 00:11:07 -0400

    Remove dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/checklists/{checklist}', function (Request $request) {
         // TODO: checklist belongs to currentTeam
         $name = Checklist::find($request->checklist)->name;
 
         return view('tasks', compact('name'));
     })->name('checklist.show');
+
     Route::view('/checklists', 'checklists')->name('checklists');
 });

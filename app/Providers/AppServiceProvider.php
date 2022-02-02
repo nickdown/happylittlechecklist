1aa57d9af12c3ace573c24e56dbb7ed4343ab3f8
nick <nick@nickdown.com>
2022-02-02 05:52:37 -0500

    configure Jetstream models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Jetstream\Jetstream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Jetstream::useUserModel('App\Models\User');
+        Jetstream::useTeamModel('App\Models\Team');
+        Jetstream::useMembershipModel('App\Models\Membership');
     }
 }
